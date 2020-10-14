40e28fce90c840c56dd879b0c9753dd3b7e14586
Burima Evgenyi <cuksmail@gmail.com>
2020-10-14 17:54:51 +0500

    solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let letterIndex = 0;
+    while(expr.length > letterIndex * 10){
+        const startPosition = letterIndex * 10;
+        const endPosition = startPosition + 10;
+
+        const letterBin = expr.slice(startPosition,  endPosition);
+        const morseCode = binToMorse(letterBin);
+        const letter = morseCode === '**********' ? ' ' : MORSE_TABLE[morseCode];
+        result += letter;
+        letterIndex++;
+    }
+    return result;
+
+    function binToMorse(binaryCode){
+        if(binaryCode.includes('*')) return binaryCode;
+        let morse = '';
+        binaryCode = binaryCode.split('');
+        while (binaryCode.length){
+            if (!binaryCode.includes('1')) {
+                return morse;
+            }
+            const signBin = [];
+            signBin.push(binaryCode.pop());
+            signBin.push(binaryCode.pop());
+            const code = signBin.reverse().join('');
+            morse = `${code == '10' ? '.' : '-'}${morse}`;
+        }
+        return morse;
+    }
 }
 
 module.exports = {
